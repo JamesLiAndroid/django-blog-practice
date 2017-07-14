8cc0c342e350437caeda95df27b1b8472c2ed241
JamesLiAndroid <lsy1234567@163.com>
2017-07-14 21:56:08 +0800

    解决-created_time的问题，完成首页模板编写

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from .models import Post
 
 def index(request):
-    post_list = Post.objects.all().order_by('-created_time') # '-'表示逆序！不加代表正序！
-    return render(request, 'blog/index.html', context = {
-        'post_list': post_list
-    })
+    """
+    -created_time
+    post_list = Post.objects.all().order_by('-created_time') #
+    """
+    post_list = Post.objects.all().order_by('-create_time') # '-'表示逆序！不加代表正序！
+    return render(request, 'blog/index.html', context = {'post_list': post_list})
