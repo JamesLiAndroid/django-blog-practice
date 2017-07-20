a6411711e6cd19a9675ecc4d17e86067b66dcec4
JamesLiAndroid <lsy1234567@163.com>
2017-07-20 16:39:27 +0800

    完成统计文章阅读量的工作

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -75,12 +75,20 @@ class Post(models.Model):
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User)
 
+    # 添加访问量的统计
+    views = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return self.title
 
     # 自定义get_absolute_url方法
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk' : self.pk})
+
+    # 增加访问量的方法
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
 
     class Meta:
         ordering = ['-create_time', 'title']
